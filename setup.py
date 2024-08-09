be594e6465880e723e0e81f7107bcadde5758955
dhodcz2 <dhodcz2@uic.edu>
2024-08-08 22:04:11 -0500

    nested under open_groundingdino

--- setup.py
+++ setup.py
@@ -37,15 +37,12 @@ class CustomInstallCommand(_install):
 with open('requirements.txt') as f:
     install_requires = f.read().splitlines()
 
-# find_packages('.')
-
 setup(
     name="open_groundingdino",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.11",
     ],
-    # packages=packages,
     python_requires='>=3.11',
     include_package_data=True,
     cmdclass={
@@ -53,4 +50,5 @@ setup(
         'install': CustomInstallCommand,
     },
     install_requires=install_requires,
+    entry_points={},
 )
