5e5959fd7ca6754986d78d6a57e0a74bdd0d8bae
dhodcz2 <dhodcz2@uic.edu>
2024-08-19 21:42:33 -0500

    ensure_torch_installed

--- setup.py
+++ setup.py
@@ -5,32 +5,44 @@ import os
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py as _build_py
 from setuptools.command.install import install as _install
+from pkg_resources import get_distribution, DistributionNotFound
+import sys
+
+def ensure_torch_installed():
+    try:
+        # Check if torch is already installed
+        get_distribution('torch')
+    except DistributionNotFound:
+        # If not installed, install torch
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'torch'])
 
 class CustomBuildCommand(_build_py):
     def run(self):
+        ensure_torch_installed()
         # Run the nested setup.py
         self.run_subprocess('open_groundingdino/models/GroundingDINO/ops')
         super().run()
 
     def run_subprocess(self, directory):
         subprocess.check_call(
-            ['python', './setup.py', 'build', 'install'], cwd=directory
+            [sys.executable, './setup.py', 'build', 'install'], cwd=directory
         )
         subprocess.check_call(
-            ['python', 'test.py'], cwd=directory
+            [sys.executable, 'test.py'], cwd=directory
         )
 
 class CustomInstallCommand(_install):
     def run(self):
+        ensure_torch_installed()
         self.run_subprocess('open_groundingdino/models/GroundingDINO/ops')
         super().run()
 
     def run_subprocess(self, directory):
         subprocess.check_call(
-            ['python', './setup.py', 'build', 'install'], cwd=directory
+            [sys.executable, './setup.py', 'build', 'install'], cwd=directory
         )
         subprocess.check_call(
-            ['python', 'test.py'], cwd=directory
+            [sys.executable, 'test.py'], cwd=directory
         )
 
 with open('requirements.txt') as f:
